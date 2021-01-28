336dd9b9860d244f8fd781f4a3222c1ebecc3f15
Manuel Duran <mduran85@gmail.com>
2021-01-28 13:40:41 -0600

    refactored structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import AppEntry from './screens/AppEntry'
+import AppEntry from './entries/AppEntry'
 import { Layout } from './styles'
 import { Provider } from 'react-redux'
 import store from './redux/store'
--- src/entries/PublicEntry/index.tsx
+++ src/entries/PublicEntry/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Text from '../../components/Text'
 import { Layout } from './styles'
-import LoginScreen from '../LoginScreen'
+import LoginScreen from '../../screens/LoginScreen'
 const PublicEntry: React.FC = () => {
   return (
     <Layout>
